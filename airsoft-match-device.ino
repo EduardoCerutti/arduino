48030dcd8c02bb9992598c5a9e0969586be86b03
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2024-09-07 20:17:44 -0300

    Add resetButton and speaker voices with TMRpcm

--- airsoft-match-device.ino
+++ airsoft-match-device.ino
@@ -8,8 +8,17 @@ TMRpcm speaker;
 
 int matchFlag = 0;
 
+int team1Timer = 0;
 int team2Timer = 0;
-int team3Timer = 0;
+
+int resetPin = 3;
+int team1Pin = 5;
+int team2Pin = 6;
+
+bool tenMinFlag = true;
+bool fiveMinFlag = true;
+bool twoMinFlag = true;
+bool endGameFlag = true;
 
 String winnerTeam = "";
 
@@ -17,8 +26,9 @@ String winnerTeam = "";
 const int endGameTime = 2000;
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(resetPin, INPUT_PULLUP);
+  pinMode(team1Pin, INPUT_PULLUP);
+  pinMode(team2Pin, INPUT_PULLUP);
   Serial.begin(9600);
 
   if (!SD.begin(sdCardPort)) {
@@ -27,32 +37,76 @@ void setup() {
   }
 
   speaker.speakerPin = speakerPort;
-  speaker.setVolume(5);
+  speaker.setVolume(2);
 }
 
 void loop() {
-  int button2 = digitalRead(2);
-  int button3 = digitalRead(3);
+  int resetButton = digitalRead(resetPin);
+  int button1 = digitalRead(team1Pin);
+  int button2 = digitalRead(team2Pin);
 
-  if (button2 == LOW) matchFlag = 2;
+  if (resetButton == LOW) {
+    matchFlag = 0;
 
-  if (button3 == LOW) matchFlag = 3;
+    team1Timer = 0;
+    team2Timer = 0;
+
+    tenMinFlag = true;
+    fiveMinFlag = true;
+    twoMinFlag = true;
+    endGameFlag = true;
+
+    String winnerTeam = "";
+  }
+
+  if (button1 == LOW) {
+    matchFlag = team1Pin;
+    speaker.play("capturaAzul.wav");
+  }
+
+  if (button2 == LOW) {
+    matchFlag = team2Pin;
+    speaker.play("capturaAmarelo.wav");
+  }
 
   if (matchFlag == 0) Serial.println("Waiting for teams to get flag");
 
-  if (matchFlag == 2 && winnerTeam.length() == 0) {
+  if (matchFlag == team1Pin && winnerTeam.length() == 0) {
+    team1Timer = team1Timer + 1;
+    Serial.println(team1Timer);
+  }
+
+  if (matchFlag == team2Pin && winnerTeam.length() == 0) {
     team2Timer = team2Timer + 1;
     Serial.println(team2Timer);
   }
 
-  if (matchFlag == 3 && winnerTeam.length() == 0) {
-    team3Timer = team3Timer + 1;
-    Serial.println(team3Timer);
+  if (team1Timer >= 50000 && tenMinFlag || team2Timer >= 50000 && tenMinFlag) {
+    speaker.play("10min.wav");
+    tenMinFlag = false;
   }
 
-  if (team2Timer >= endGameTime) winnerTeam = "Team 2";
+  if (team1Timer >= 100000 && fiveMinFlag || team2Timer >= 100000 && fiveMinFlag) {
+    speaker.play("5min.wav");
+    fiveMinFlag = false;
+  }
 
-  if (team3Timer >= endGameTime) winnerTeam = "Team 3";
+  if (team1Timer >= 130000 && twoMinFlag || team2Timer >= 130000 && twoMinFlag) {
+    speaker.play("2min.wav");
+    twoMinFlag = false;
+  }
 
-  if (winnerTeam.length() > 0) Serial.println(winnerTeam);
+  if (team1Timer >= endGameTime && endGameFlag) {
+    speaker.play("endGameAzul.wav");
+    Serial.println("TEAM 1 WIN");
+    winnerTeam = "Team 1";
+    endGameFlag = false;
+  }
+
+  if (team2Timer >= endGameTime && endGameFlag) {
+    speaker.play("endGameAmarelo.wav");
+    Serial.println("TEAM 2 WIN");
+    winnerTeam = "Team 2";
+    endGameFlag = false;
+  }
 }
